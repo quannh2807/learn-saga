36cad6c1d3e718256aa0d6a79826e4fb17d8d349
quannh2807 <helgrindxxx@gmail.com>
2021-09-15 17:27:17 +0700

    Redux Saga: 13 - Setup APIs for student resource in react typescript

--- src/api/studentApi.ts
+++ src/api/studentApi.ts
@@ -1,10 +1,30 @@
+import { ListParams, ListResponse, Student } from 'types';
 import axiosClient from './axiosClient';
 
 const studentApi = {
-	getAll: () => {
+	getAll(params: ListParams): Promise<ListResponse<Student>> {
 		const url = '/students';
+		return axiosClient.get(url, { params });
+    },
 
+    getById(id: string): Promise<any> {
+		const url = `/students/${id}`;
 		return axiosClient.get(url);
+	},
+
+	add(data: Student): Promise<Student> {
+		const url = '/students';
+		return axiosClient.post(url, data);
+	},
+
+	update(data: Student): Promise<Student> {
+		const url = '/students';
+		return axiosClient.patch(url, data);
+	},
+
+	remove(id: string): Promise<any> {
+		const url = `/students/${id}`;
+		return axiosClient.delete(url);
 	},
 };
 
